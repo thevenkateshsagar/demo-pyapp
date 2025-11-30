0b41ec42272c5e416a4c8d06a76ca271c53ec5a5
venkatesh <venkatesh.sagard@gmail.com>
2025-11-30 07:17:45 +0530

    changes made

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def add_num(a, b):
+def add(a, b):
     return a + b
 
 
-def sub_num(a, b):
+def sub(a, b):
     return a-b
 
 
 if __name__ == "__main__":
-    print("Add:", add_num(10, 5))
-    print("Sub:", sub_num(10, 5))
+    print("Add:", add(10, 5))
+    print("Sub:", sub(10, 5))
